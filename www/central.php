2431dc54d530092523117372b3ed1203ab4c93a7
Tim van Dijen <tvdijen@gmail.com>
2019-08-11 12:37:14 +0200

    Fix build

--- www/central.php
+++ www/central.php
@@ -1,11 +1,13 @@
 <?php
 
-if (empty($_REQUEST['entityID'])) throw new Exception('Missing parameter [entityID]');
-if (empty($_REQUEST['return'])) throw new Exception('Missing parameter [return]');
+if (empty($_REQUEST['entityID'])) {
+    throw new \Exception('Missing parameter [entityID]');
+} elseif (empty($_REQUEST['return'])) {
+    throw new \Exception('Missing parameter [return]');
+}
 
-
-$djconfig = SimpleSAML_Configuration::getOptionalConfig('discojuice.php');
-$config = SimpleSAML_Configuration::getInstance();
+$djconfig = \SimpleSAML\Configuration::getOptionalConfig('discojuice.php');
+$config = \SimpleSAML\Configuration::getInstance();
 
 // EntityID
 $entityid = $_REQUEST['entityID'];
@@ -13,33 +15,32 @@ $entityid = $_REQUEST['entityID'];
 // Return to...
 $returnidparam = !empty($_REQUEST['returnIDParam']) ? $_REQUEST['returnIDParam'] : 'entityID';
 $href = \SimpleSAML\Utils\HTTP::addURLParameters(
-	$_REQUEST['return'],
-	array($returnidparam => '')
+    $_REQUEST['return'],
+    [$returnidparam => '']
 );
 
+$hostedConfig = [
+    // Name of service
+    $djconfig->getString('name', 'Service'),
 
-$hostedConfig = array(
-	// Name of service
-	$djconfig->getString('name', 'Service'),
-
-	$entityid,
+    $entityid,
 	
-	// Url to response
-	SimpleSAML\Module::getModuleURL('discojuice/response.html'),
+    // Url to response
+    \SimpleSAML\Module::getModuleURL('discojuice/response.html'),
 	
-	// Set of feeds to subscribe to.
-	$djconfig->getArray('feeds', array('edugain')), 
+    // Set of feeds to subscribe to.
+    $djconfig->getArray('feeds', ['edugain']), 
 	
-	$href
-);
+    $href
+];
 
 /*
-	"a.signin", "Teest Demooo",
+    "a.signin", "Teest Demooo",
     "https://example.org/saml2/entityid",
-    "' . SimpleSAML\Module::getModuleURL('discojuice/discojuice/discojuiceDiscoveryResponse.html') . '", ["kalmar"], "http://example.org/login?idp="
+    "' . \SimpleSAML\Module::getModuleURL('discojuice/discojuice/discojuiceDiscoveryResponse.html') . '", ["kalmar"], "http://example.org/login?idp="
 */
 
-$t = new SimpleSAML_XHTML_Template($config, 'discojuice:central.tpl.php');
+$t = new \SimpleSAML\XHTML\Template($config, 'discojuice:central.tpl.php');
 $t->data['hostedConfig'] = $hostedConfig;
 $t->data['enableCentralStorage'] = $djconfig->getBoolean('enableCentralStorage', true);
 $t->data['additionalFeeds'] = $djconfig->getArray('additionalFeeds', null);
