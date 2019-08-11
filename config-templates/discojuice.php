3a5d54c814b39030088cf8737b842274f439051c
Tim van Dijen <tvdijen@gmail.com>
2019-08-11 14:22:34 +0200

    Prepare for SSP 2.0

--- config-templates/discojuice.php
+++ config-templates/discojuice.php
@@ -12,7 +12,7 @@ $config = [
      * You may visit https://static.discojuice.org/feeds/
      *
      * to review the available feed identifiers.
-     * You may choose to not use any of the provider feed, by setting this to an 
+     * You may choose to not use any of the provider feed, by setting this to an
      * empty array: []
      */
     'feeds' => ['edugain'],
@@ -24,12 +24,12 @@ $config = [
 
     /*
      * If you set this value to true, the module will contact discojuice.org to read and write cookies.
-     * If you enable this, you will also need to get your host accepted in the access control list of 
+     * If you enable this, you will also need to get your host accepted in the access control list of
      * discojuice.org
      *
      * The response URL of your service, similar to:
      *
-     *     https://sp.example.org/simplesaml/module.php/discojuice/response.html	
+     *     https://sp.example.org/simplesaml/module.php/discojuice/response.html
      *
      * will need to be registered at discojuice.org. If your response URL is already registered in the metadata
      * of one of the federation feeds at discojuice.org, you should already have access.
--- www/central.php
+++ www/central.php
@@ -24,13 +24,13 @@ $hostedConfig = [
     $djconfig->getString('name', 'Service'),
 
     $entityid,
-	
+
     // Url to response
     \SimpleSAML\Module::getModuleURL('discojuice/response.html'),
-	
+
     // Set of feeds to subscribe to.
-    $djconfig->getArray('feeds', ['edugain']), 
-	
+    $djconfig->getArray('feeds', ['edugain']),
+
     $href
 ];
 
@@ -40,11 +40,18 @@ $hostedConfig = [
     "' . \SimpleSAML\Module::getModuleURL('discojuice/discojuice/discojuiceDiscoveryResponse.html') . '", ["kalmar"], "http://example.org/login?idp="
 */
 
-$t = new \SimpleSAML\XHTML\Template($config, 'discojuice:central.tpl.php');
+header('P3P:CP="IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT"');
+
+$t = new \SimpleSAML\XHTML\Template($config, 'discojuice:central.twig');
 $t->data['hostedConfig'] = $hostedConfig;
 $t->data['enableCentralStorage'] = $djconfig->getBoolean('enableCentralStorage', true);
 $t->data['additionalFeeds'] = $djconfig->getArray('additionalFeeds', null);
-$t->show();
-
-
-
+$t->data['script'] = '$("document").ready(function() {
+    var djc = DiscoJuice.Hosted.getConfig(' .
+        json_encode($t->data['hostedConfig'][0]) . "," .
+        json_encode($t->data['hostedConfig'][1]) . "," .
+        json_encode($t->data['hostedConfig'][2]) . "," .
+        json_encode($t->data['hostedConfig'][3]) . "," .
+        json_encode($t->data['hostedConfig'][4]) .
+    ');';
+$t->send();
